9bffb6dffa97caa8ef227ae6d981988f11a92bcf
Joao Marco <joaomarcobarros@gmail.com>
2024-03-24 13:38:31 -0300

    adding user entity getters and setters

--- src/domain/entities/user.ts
+++ src/domain/entities/user.ts
@@ -12,4 +12,58 @@ export class User {
   constructor(props: UserProps) {
     this.props = { ...props, reminders: props.reminders || [] };
   }
+
+  updateName(param: string) {
+    this.name = param;
+  }
+
+  private set name(param: string) {
+    this.props.name = param;
+  }
+
+  get name(): string {
+    return this.props.name;
+  }
+
+  /////
+
+  updateEmail(param: string) {
+    this.email = param;
+  }
+
+  private set email(param: string) {
+    this.props.email = param;
+  }
+
+  get email(): string {
+    return this.props.email;
+  }
+
+  /////
+
+  updatePlan(param: PlansType) {
+    this.name = param;
+  }
+
+  private set plan(param: PlansType) {
+    this.props.plan = param;
+  }
+
+  get plan(): PlansType {
+    return this.props.plan;
+  }
+
+  /////
+
+  updateReminders(param: string[]) {
+    this.reminders = param;
+  }
+
+  private set reminders(param: string[]) {
+    this.props.reminders = param;
+  }
+
+  get reminders(): string[] {
+    return this.props.reminders;
+  }
 }
